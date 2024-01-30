6e9c6b5bf545b6b3373cd11641e2dbf437554b6c
mvimal2607 <mvimal2607@gmail.com>
2024-01-30 04:13:20 +0000

    sm8350-common: welcome to awaken
    Signed-off-by: mvimal2607 <mvimal2607@gmail.com>

--- common.mk
+++ common.mk
@@ -320,7 +320,7 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-aosp
+    $(LOCAL_PATH)/overlay-awaken
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
